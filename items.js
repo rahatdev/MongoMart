b982759d259795065c5c36d275d780f2c7eefa88
rahat <rahat.forja@gmail.com>
2017-12-12 00:20:40 -0600

    4 - items.addReview() completed.

--- items.js
+++ items.js
@@ -296,12 +296,9 @@ function ItemDAO(database) {
          *
          */
 
-        console.log('searching for item: ' + itemId);
-
         this.db.collection('item').find({ _id: itemId })
             .limit(1)
             .next((err, result) => {
-                console.log(result);
                 callback(result);
             })
 
@@ -352,13 +349,16 @@ function ItemDAO(database) {
 
         // TODO replace the following two lines with your code that will
         // update the document with a new review.
-        var doc = this.createDummyItem();
-        doc.reviews = [reviewDoc];
+        this.db.collection('item').updateOne({ _id: itemId }, { $push: {reviews: reviewDoc}}, (err, doc) => {
+            if(err) console.log(err);
+            console.log(doc);
+            callback(doc);
+        } )
 
         // TODO Include the following line in the appropriate
         // place within your code to pass the updated doc to the
         // callback.
-        callback(doc);
+        
     }
 
 
